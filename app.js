e8e2ed04e0396b5143174d9f241ad1f42347e423
parmezano <danilchick1612@gmail.com>
2022-01-25 14:09:05 +0200

    final version

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const path = require('path')
+const {v4} = require('uuid')
+const app = express();
+
+app.use(express.json())
+
+CONTACTS=[
+    {id: v4(), name: 'Danila', value:'+871263548734', marked: false}
+]
+
+///GET
+app.get('/api/contacts', (req, res)=>{
+    res.status(200).json(CONTACTS)
+})
+
+///POST
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}; ///// почему при задании айди при помощи Date.now() не работает обработка пут и делит на стороне сервера
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
+
+///DELETE
+app.delete(`/api/contacts/:id`, (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.json({message: 'Контакт был удалён!'})
+})
+
+///PUT
+app.put('/api/contacts/:id', (req,res) => {
+    const indx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[indx] = req.body
+    res.json(CONTACTS[indx])
+})
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client', 'index.html'))
+})
+
+app.listen(3000, ()=>{
+    console.log('Server has been started at port 3000...')
+})
---
+++ client/frontend.js
@@ -0,0 +1,66 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+
+new Vue({
+    el: "#app", //казываем при помощи el корневой элемент
+data(){
+        return{
+            form:{
+                name: '',
+                value: ''
+            },
+            contacts:[]
+
+        }
+},
+    computed:{
+        canCreate(){
+            return this.form.name.trim() && this.form.value.trim()
+        }
+
+    },
+
+    methods:{
+        async createContact(){
+            const {...contact} = this.form  // создаём объект с нужными нам значениями
+
+            const newContact = await request ('/api/contacts', 'POST', contact)
+
+            this.contacts.push(newContact)  //добавляем введенные данные в массив для хранения
+
+            this.form.name = this.form.value = ''  //очищаем поля для ввода
+        },
+       async markContact(id){
+            const contact = this.contacts.find(contact => contact.id === id)
+           const updated = await request(`/api/contacts/${id}`, 'PUT', {...contact, marked: true})
+           contact.marked = updated.marked
+        },
+        async deleteContact(id){
+           await request (`/api/contacts/${id}`, 'DELETE')
+            this.contacts = this.contacts.filter(contact => contact.id !== id)
+        }
+    },
+    async mounted(){
+        this.contacts = await request('/api/contacts')
+    }
+})
+
+
+async function request(url, method = 'GET', data = null) {
+    try{
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+        const response = await fetch(url, {
+            method,
+            headers,
+            body,
+        })
+        return await response.json()
+    } catch (e) {
+        console.warn('Error: ', e)
+    }
+}
